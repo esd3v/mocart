424448d3d1f8281b4114b299ce6c61350faedb07
ES <esd3v@proton.me>
2016-10-10 18:44:41 +0700

    Add 'slider' placeholder. Add 'deals' block.

--- src/js/custom.js
+++ src/js/custom.js
@@ -10,7 +10,7 @@ function GetRandomColor() {
 		hex += chars[(Math.random() * 16) | 0];
 	}
 	return hex;
-};
+}
 
 // Accordion
 ////////////////////////////////////////////////////////
@@ -28,4 +28,35 @@ if (DEBUG) {
 	$.each($("div,svg,a"), function (index, item) {
 		$(item).attr('style', "background-color: " + GetRandomColor());
 	});
-};
+}
+
+// Countdown
+////////////////////////////////////////////////////////
+(function CountDown()
+{
+	var setTime = [0,1,15,41];
+	var setTotal = ((((((setTime[0]*24)+setTime[1])*60)+setTime[2])*60)+setTime[3])*1000;
+	var endTime = new Date(Date.now() + setTotal);
+
+	(function Decrease() {
+		var delay = 0; // Prevent delay for the first execution
+		setTimeout(function() {
+			var remTime = Date.parse(endTime) - Date.parse(new Date());
+			// var days = Math.floor(remTime / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((remTime / (1000 * 60 * 60)) % 24);
+			var minutes = Math.floor((remTime / 1000 / 60) % 60);
+			var seconds = Math.floor((remTime / 1000) % 60);
+
+			// document.querySelector('.deals__days').innerHTML = days;
+			document.querySelector('.countDown__digit--hours').innerHTML = ('0' + hours).slice(-2);
+			document.querySelector('.countDown__digit--minutes').innerHTML = ('0' + minutes).slice(-2);
+			document.querySelector('.countDown__digit--seconds').innerHTML = ('0' + seconds).slice(-2);
+			if (delay <= 0) { // Next executions will have 1sec delay
+				delay = 1000;
+			}
+			if (remTime > 0) {
+				Decrease();
+			}
+		},delay);
+	})();
+})();
